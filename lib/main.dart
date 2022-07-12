6217709d9a88c3d29dece59240cbabc658db1a10
Anderson Mendes <developer.andersonmendes@gmail.com>
2022-07-11 22:40:38 -0300

    Aula 225 - Finalizada

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/models/product_list.dart';
 import 'package:shop/pages/product_detail_page.dart';
 import 'package:shop/pages/products_overview_page.dart';
 import 'package:shop/utils/app_routes.dart';
@@ -15,18 +17,21 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     final theme = ThemeData();
 
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: theme.copyWith(
-          colorScheme: theme.colorScheme.copyWith(
-            primary: Colors.purple,
-            secondary: Colors.deepOrangeAccent,
-          ),
-          textTheme: theme.textTheme
-              .copyWith(headline6: const TextStyle(fontFamily: 'Lato'))),
-      home: ProductOverviewPage(),
-      debugShowCheckedModeBanner: false,
-      routes: {AppRoutes.productDetail: (ctx) => ProductDetailPage()},
+    return ChangeNotifierProvider(
+      create: (_) => ProductList(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: theme.copyWith(
+            colorScheme: theme.colorScheme.copyWith(
+              primary: Colors.purple,
+              secondary: Colors.deepOrangeAccent,
+            ),
+            textTheme: theme.textTheme
+                .copyWith(headline6: const TextStyle(fontFamily: 'Lato'))),
+        home: ProductOverviewPage(),
+        debugShowCheckedModeBanner: false,
+        routes: {AppRoutes.productDetail: (ctx) => ProductDetailPage()},
+      ),
     );
   }
 }
--- lib/pages/products_overview_page.dart
+++ lib/pages/products_overview_page.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:shop/components/product_item.dart';
 import 'package:shop/data/dummy_data.dart';
+import 'package:shop/models/product_list.dart';
 
 import '../models/product.dart';
 
 class ProductOverviewPage extends StatelessWidget {
-  final List<Product> loadedProducts = dummyProducts;
-
   ProductOverviewPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final provider = Provider.of<ProductList>(context);
+    final List<Product> loadedProducts = provider.items;
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Minha loja'),
