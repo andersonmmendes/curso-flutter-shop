decdc4bb4fb330128404e63f17b58ed309ff5272
Anderson Mendes <anderson.mendes@guardiaodigital.com.br>
2023-06-09 14:29:39 -0300

    Aula 237 - Finalizada

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,14 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/models/cart.dart';
 
 class CartPage extends StatelessWidget {
   const CartPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final Cart cart = Provider.of(context);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Carrinho'),
       ),
+      body: Column(children: [
+        Card(
+          margin: const EdgeInsets.all(25),
+          child: Padding(
+            padding: const EdgeInsets.all(10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text(
+                  'Total',
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
+                ),
+                const SizedBox(
+                  width: 10,
+                ),
+                Chip(
+                  label: Text(
+                    'R\$${cart.totalAmount.toStringAsFixed(2)}',
+                    style: const TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                  backgroundColor: Theme.of(context).colorScheme.primary,
+                ),
+                const Spacer(),
+                TextButton(
+                  onPressed: () {},
+                  style: TextButton.styleFrom(
+                    textStyle: TextStyle(
+                      color: Theme.of(context).colorScheme.primary,
+                    ),
+                  ),
+                  child: const Text('Comprar'),
+                )
+              ],
+            ),
+          ),
+        )
+      ]),
     );
   }
 }
