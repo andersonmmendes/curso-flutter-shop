723b5b6700a0c9aaceb3160841785bba303030ee
Anderson Mendes <anderson.mendes@guardiaodigital.com.br>
2023-06-12 13:57:18 -0300

    Aula 238 - Finalizada

---
+++ lib/components/cart_item.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:shop/models/cart_item.dart';
+
+class CartItemWidget extends StatelessWidget {
+  final CartItem cartItem;
+
+  const CartItemWidget({super.key, required this.cartItem});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(cartItem.name);
+  }
+}
--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shop/components/cart_item.dart';
 import 'package:shop/models/cart.dart';
 
 class CartPage extends StatelessWidget {
@@ -8,52 +9,61 @@ class CartPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final Cart cart = Provider.of(context);
+    final items = cart.items.values.toList();
 
     return Scaffold(
       appBar: AppBar(
         title: const Text('Carrinho'),
       ),
-      body: Column(children: [
-        Card(
-          margin: const EdgeInsets.all(25),
-          child: Padding(
-            padding: const EdgeInsets.all(10),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Text(
-                  'Total',
-                  style: TextStyle(
-                    fontSize: 20,
-                  ),
-                ),
-                const SizedBox(
-                  width: 10,
-                ),
-                Chip(
-                  label: Text(
-                    'R\$${cart.totalAmount.toStringAsFixed(2)}',
-                    style: const TextStyle(
-                      color: Colors.white,
+      body: Column(
+        children: [
+          Card(
+            margin: const EdgeInsets.all(25),
+            child: Padding(
+              padding: const EdgeInsets.all(10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    'Total',
+                    style: TextStyle(
+                      fontSize: 20,
                     ),
                   ),
-                  backgroundColor: Theme.of(context).colorScheme.primary,
-                ),
-                const Spacer(),
-                TextButton(
-                  onPressed: () {},
-                  style: TextButton.styleFrom(
-                    textStyle: TextStyle(
-                      color: Theme.of(context).colorScheme.primary,
-                    ),
+                  const SizedBox(
+                    width: 10,
                   ),
-                  child: const Text('Comprar'),
-                )
-              ],
+                  Chip(
+                    label: Text(
+                      'R\$${cart.totalAmount.toStringAsFixed(2)}',
+                      style: const TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                    backgroundColor: Theme.of(context).colorScheme.primary,
+                  ),
+                  const Spacer(),
+                  TextButton(
+                    onPressed: () {},
+                    style: TextButton.styleFrom(
+                      textStyle: TextStyle(
+                        color: Theme.of(context).colorScheme.primary,
+                      ),
+                    ),
+                    child: const Text('Comprar'),
+                  )
+                ],
+              ),
             ),
           ),
-        )
-      ]),
+          Expanded(
+            child: ListView.builder(
+              itemCount: items.length,
+              itemBuilder: (ctx, i) => CartItemWidget(cartItem: items[i]),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
