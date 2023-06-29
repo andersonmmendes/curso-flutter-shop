eded6baad9216fa1ac92f4c448be6d875a959114
Anderson Mendes <anderson.mendes@guardiaodigital.com.br>
2023-06-29 14:18:56 -0300

    Aula 283 - Finalizada

--- lib/models/product_list.dart
+++ lib/models/product_list.dart
@@ -8,8 +8,8 @@ import 'package:http/http.dart' as http;
 import 'product.dart';
 
 class ProductList with ChangeNotifier {
-  final _url =
-      'https://shop-cod3r-c6c00-default-rtdb.firebaseio.com/products.json';
+  final _baseUrl =
+      'https://shop-cod3r-c6c00-default-rtdb.firebaseio.com/products';
 
   List<Product> _items = [];
 
@@ -24,7 +24,7 @@ class ProductList with ChangeNotifier {
   Future<void> loadProducts() async {
     _items.clear();
 
-    final response = await http.get(Uri.parse(_url));
+    final response = await http.get(Uri.parse('$_baseUrl.json'));
 
     if (response.body == 'null') return;
 
@@ -65,7 +65,7 @@ class ProductList with ChangeNotifier {
 
   Future<void> addProduct(Product product) async {
     final response = await http.post(
-      Uri.parse(_url),
+      Uri.parse('$_baseUrl.json'),
       body: jsonEncode(
         {
           "name": product.name,
@@ -93,15 +93,25 @@ class ProductList with ChangeNotifier {
     notifyListeners();
   }
 
-  Future<void> updateProduct(Product product) {
+  Future<void> updateProduct(Product product) async {
     int index = _items.indexWhere((p) => p.id == product.id);
 
     if (index >= 0) {
+      await http.patch(
+        Uri.parse('$_baseUrl/${product.id}.json'),
+        body: jsonEncode(
+          {
+            "name": product.name,
+            "description": product.description,
+            "price": product.price,
+            "imageUrl": product.imageUrl
+          },
+        ),
+      );
+
       _items[index] = product;
       notifyListeners();
     }
-
-    return Future.value();
   }
 
   void removeProduct(Product product) {
