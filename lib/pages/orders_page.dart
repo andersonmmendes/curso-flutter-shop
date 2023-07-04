cbe1f63d6ec60482371baa254e282e77ac6048ce
Anderson Mendes <anderson.mendes@guardiaodigital.com.br>
2023-07-04 14:32:23 -0300

    Aula 291 - Finalizada

--- lib/pages/orders_page.dart
+++ lib/pages/orders_page.dart
@@ -4,49 +4,35 @@ import 'package:shop/components/app_drawer.dart';
 import 'package:shop/components/order.dart';
 import 'package:shop/models/order_list.dart';
 
-class OrdersPage extends StatefulWidget {
+class OrdersPage extends StatelessWidget {
   const OrdersPage({super.key});
 
   @override
-  State<OrdersPage> createState() => _OrdersPageState();
-}
-
-class _OrdersPageState extends State<OrdersPage> {
-  bool _isLoading = true;
-
-  Future<void> _refreshOrders(BuildContext context) {
-    return Provider.of<OrderList>(context, listen: false).loadOrders();
-  }
-
-  @override
-  void initState() {
-    super.initState();
-
-    _refreshOrders(context).then((_) {
-      setState(() => _isLoading = false);
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
-    final OrderList orders = Provider.of(context);
-
     return Scaffold(
       appBar: AppBar(
         title: const Text('Meus pedidos'),
       ),
       drawer: const AppDrawer(),
-      body: _isLoading
-          ? const Center(
+      body: FutureBuilder(
+        future: Provider.of<OrderList>(context, listen: false).loadOrders(),
+        builder: (ctx, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
               child: CircularProgressIndicator(),
-            )
-          : RefreshIndicator(
-              onRefresh: () => _refreshOrders(context),
-              child: ListView.builder(
+            );
+          } else if (snapshot.error != null) {
+            return const Center(child: Text('Ocorreu um erro!'));
+          } else {
+            return Consumer<OrderList>(
+              builder: (ctx, orders, child) => ListView.builder(
                 itemCount: orders.itemsCount,
                 itemBuilder: (ctx, i) => OrderWidget(order: orders.items[i]),
               ),
-            ),
+            );
+          }
+        },
+      ),
     );
   }
 }
