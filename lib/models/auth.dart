aef83e7cbc36ce8bd132abfb7325bcd1d61cbb1a
Anderson Mendes <anderson.mendes@guardiaodigital.com.br>
2023-07-10 14:14:14 -0300

    Aula 300 - Finalizada

---
+++ lib/models/auth.dart
@@ -0,0 +1,22 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class Auth with ChangeNotifier {
+  static const _url =
+      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyB-gcet9_lnK4zrbMg66l1kb0C4O5nUcpM';
+
+  Future<void> signup(String email, String password) async {
+    final response = await http.post(
+      Uri.parse(_url),
+      body: jsonEncode({
+        'email': email,
+        'password': password,
+        'returnSecureToken': true,
+      }),
+    );
+
+    print(jsonDecode(response.body));
+  }
+}
