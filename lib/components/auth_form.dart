fdb417fd8b552e2d0809bc45d8530c5d1c087106
Anderson Mendes <anderson.mendes@guardiaodigital.com.br>
2023-07-10 14:22:41 -0300

    Aula 301 - Finalizada

--- lib/components/auth_form.dart
+++ lib/components/auth_form.dart
@@ -50,6 +50,7 @@ class _AuthFormState extends State<AuthForm> {
     Auth auth = Provider.of(context, listen: false);
 
     if (_isLogin()) {
+      await auth.login(_authData['email']!, _authData['password']!);
     } else {
       await auth.signup(_authData['email']!, _authData['password']!);
     }
--- lib/models/auth.dart
+++ lib/models/auth.dart
@@ -7,9 +7,16 @@ class Auth with ChangeNotifier {
   static const _url =
       'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyB-gcet9_lnK4zrbMg66l1kb0C4O5nUcpM';
 
-  Future<void> signup(String email, String password) async {
+  Future<void> _authenticate(
+    String email,
+    String password,
+    String urlFragment,
+  ) async {
+    final url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:$urlFragment?key=AIzaSyB-gcet9_lnK4zrbMg66l1kb0C4O5nUcpM';
+
     final response = await http.post(
-      Uri.parse(_url),
+      Uri.parse(url),
       body: jsonEncode({
         'email': email,
         'password': password,
@@ -18,5 +25,13 @@ class Auth with ChangeNotifier {
     );
 
     print(jsonDecode(response.body));
+  }
+
+  Future<void> signup(String email, String password) async {
+    _authenticate(email, password, 'signUp');
+  }
+
+  Future<void> login(String email, String password) async {
+    _authenticate(email, password, 'signInWithPassword');
   }
 }
