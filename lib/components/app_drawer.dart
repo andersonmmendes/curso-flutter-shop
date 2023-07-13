6185bb99388509a94f3c4117689c157a5dc14891
Anderson Mendes <anderson.mendes@guardiaodigital.com.br>
2023-07-13 14:11:00 -0300

    Aula 309 - Finalizada

--- lib/components/app_drawer.dart
+++ lib/components/app_drawer.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/models/auth.dart';
 import 'package:shop/utils/app_routes.dart';
 
 class AppDrawer extends StatelessWidget {
@@ -35,6 +37,15 @@ class AppDrawer extends StatelessWidget {
             title: const Text('Gerenciar produtos'),
             onTap: () {
               Navigator.of(context).pushReplacementNamed(AppRoutes.products);
+            },
+          ),
+          const Divider(),
+          ListTile(
+            leading: const Icon(Icons.exit_to_app),
+            title: const Text('Sair'),
+            onTap: () {
+              Provider.of<Auth>(context, listen: false).logout();
+              Navigator.of(context).pushReplacementNamed(AppRoutes.authOrHome);
             },
           )
         ],
