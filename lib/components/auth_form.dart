6a60d45e27d2bfa2d1b9293cf490af536ba5059c
Anderson Mendes <developer.andersonmendes@gmail.com>
2024-01-04 20:17:25 -0300

    Aula 318 - Finalizada

--- lib/components/auth_form.dart
+++ lib/components/auth_form.dart
@@ -122,14 +122,13 @@ class _AuthFormState extends State<AuthForm>
     return Card(
       elevation: 8,
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-      child: AnimatedBuilder(
-        animation: _heightAnimation!,
-        builder: (context, child) => Container(
-            padding: const EdgeInsets.all(16),
-            // height: _isLogin() ? 310 : 400,
-            height: _heightAnimation?.value.height ?? (_isLogin() ? 310 : 400),
-            width: deviceSize.width * 0.75,
-            child: child),
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.easeIn,
+        padding: const EdgeInsets.all(16),
+        height: _isLogin() ? 310 : 400,
+        // height: _heightAnimation?.value.height ?? (_isLogin() ? 310 : 400),
+        width: deviceSize.width * 0.75,
         child: Form(
           key: _formKey,
           child: Column(
