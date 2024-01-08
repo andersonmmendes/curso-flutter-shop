058271c26058e65d7d42baa3822682e623c35e40
Anderson Mendes <developer.andersonmendes@gmail.com>
2024-01-08 19:49:49 -0300

    Aula 326 - Finalizada

--- lib/components/app_drawer.dart
+++ lib/components/app_drawer.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop/models/auth.dart';
+import 'package:shop/pages/orders_page.dart';
 import 'package:shop/utils/app_routes.dart';
+import 'package:shop/utils/custom_route.dart';
 
 class AppDrawer extends StatelessWidget {
   const AppDrawer({super.key});
--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ import 'package:shop/pages/product_detail_page.dart';
 import 'package:shop/pages/product_form_page.dart';
 import 'package:shop/pages/products_page.dart';
 import 'package:shop/utils/app_routes.dart';
+import 'package:shop/utils/custom_route.dart';
 
 void main() {
   runApp(const MyApp());
@@ -62,6 +63,12 @@ class MyApp extends StatelessWidget {
           ),
           textTheme: theme.textTheme.copyWith(
             headline6: const TextStyle(fontFamily: 'Lato'),
+          ),
+          pageTransitionsTheme: PageTransitionsTheme(
+            builders: {
+              TargetPlatform.android: CustomPageTransitionsBuilder(),
+              TargetPlatform.iOS: CustomPageTransitionsBuilder(),
+            },
           ),
         ),
         debugShowCheckedModeBanner: false,
